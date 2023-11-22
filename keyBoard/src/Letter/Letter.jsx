3ce83daa65bcf7cb4058f7b0212af3c871fb68a5
rachelibeckerman <Hand4c3@mbjcomp.org.il>
2023-11-22 15:42:21 +0200

    open letter

---
+++ keyBoard/src/Letter/Letter.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Letter(props){
+
+    return (
+    <>
+        <button className={props.latter}>{props.latter}</button>
+    </>
+    );
+    }
+
+    export default Letter
